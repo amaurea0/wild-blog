73873f604fcc8ddbe52cfab5d67f2e73ab466c78
Amaurea <aurore.chatagnier@gmail.com>
2017-07-12 11:18:04 +0200

    affichage de l'heure dans la navbar

--- public/js/components/common/navbar.js
+++ public/js/components/common/navbar.js
@@ -1,9 +1,13 @@
+import moment from 'moment';
+
 let navbar = {
     templateUrl: 'js/components/common/navbar.html',
-    controller: ['UsersService', '$state', function(UsersService, $state) {
+    controller: ['UsersService', '$state', function (UsersService, $state) {
         'use strict'
         angular.extend(this, {
             $onInit() {
+                console.log(moment().format("MMM Do YY"));
+                this.moment = moment().format('MMMM Do YYYY, h:mm:ss a');
                 UsersService.getCurrent().then((user) => {
                     this.user = user
                 }).catch((err) => {
